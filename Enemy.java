5d5e5fba31c7f1167e5443636c33dcea78cadcf4
DeltalpLol <luckydeltalp@gmail.com>
2024-11-13 18:57:37 +0100

    Testing again bruv

--- Enemy.java
+++ Enemy.java
@@ -12,7 +12,8 @@ public class Enemy extends Actor
      * Act - do whatever the Enemy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public static int health = 5;
+    public int health = 10;
+    
     public void act()
     {
         // Add your action code here.
